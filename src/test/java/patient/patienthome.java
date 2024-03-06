82b064a35e1521cf7dfa21af7bd333c494dcdcfe
Dell <Dell@DESKTOP-63VPLUC>
2024-03-06 16:24:46 +0530

    New Test case added

---
+++ src/test/java/patient/patienthome.java
@@ -0,0 +1,22 @@
+package patient;
+
+import org.testng.annotations.Test;
+
+public class patienthome {
+	
+	@Test
+	public void tc_01()
+	{
+		System.out.println("patient dash page displayed");
+		
+	}
+	
+	@Test
+	public void tc_02()
+	{
+		System.out.println("patient dash page not displayed");
+	}
+	
+	
+
+}
